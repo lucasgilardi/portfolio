8b35741743484941691b951991013cab57564500
Lucas Gilardi <lcsgilardi@gmail.com>
2022-09-24 15:28:39 -0300

    Mejoras form

--- js/main.js
+++ js/main.js
@@ -152,3 +152,83 @@ themeButton.addEventListener("click", () =>{
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 })
+
+/*Form*/
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+const response = document.getElementById("response");
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nombreValue = nombre.value.trim();
+    const emailValue = email.value.trim();
+    const mensajeValue = mensaje.value.trim();
+
+    if(nombreValue === ''){
+        setError(nombre, 'Por favor indica tu nombre.');
+    }else{
+        setSuccess(nombre);
+    }
+
+    if(emailValue === ''){
+        setError(email, 'Por favor indica tu email.');
+    }else if (!isValidEmail(emailValue)){
+        setError(email, 'Por favor ingrese un email válido.');
+    }else{
+        setSuccess(email);
+    }
+
+    if (mensajeValue === ''){
+        setError(mensaje, 'Por favor ingrese su consulta.');
+    }else{
+        setSuccess(mensaje);
+    }
+}
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    validateInputs();
+
+    fetch("https://formsubmit.co/ajax/lcsgilardi@gmail.com",{
+        method:'POST',
+        body:new FormData(e.target)
+    })
+    .then(res => res.ok ? res.json():Promise.reject(res))
+    .then(json => {
+        console.log(json);
+        response.innerHTML = `<p>${json.message}</p>`
+        form.reset();
+    })
+    .catch(err => {
+        console.log(err);
+        let message = err.statusText || "Ocurrió un error, intenta nuevamente";
+        response.innerHTML = `<p>Error ${err.status}: ${message}</p>`
+    })
+    .finally(() => setTimeout(() =>{
+        response.innerHTML = "";
+    }, 3000))
+})
