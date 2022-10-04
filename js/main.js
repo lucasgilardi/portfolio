421b8936f1b51b37744d96d8297b312be9554996
Lucas Gilardi <lcsgilardi@gmail.com>
2022-10-04 19:37:08 -0300

    Agregado proyecto a portfolio

--- js/main.js
+++ js/main.js
@@ -154,64 +154,64 @@ themeButton.addEventListener("click", () =>{
 })
 
 /*Form*/
-const form = document.getElementById("form");
-const nombre = document.getElementById("nombre");
-const email = document.getElementById("email");
-const mensaje = document.getElementById("mensaje");
-const response = document.getElementById("response");
+// const form = document.getElementById("form");
+// const nombre = document.getElementById("nombre");
+// const email = document.getElementById("email");
+// const mensaje = document.getElementById("mensaje");
+// const response = document.getElementById("response");
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+// const setError = (element, message) => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
-}
+//     errorDisplay.innerText = message;
+//     inputControl.classList.add('error');
+//     inputControl.classList.remove('success');
+// }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+// const setSuccess = element => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
 
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-}
+//     errorDisplay.innerText = '';
+//     inputControl.classList.add('success');
+//     inputControl.classList.remove('error');
+// }
 
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+// const isValidEmail = email => {
+//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+// }
 
-const validateInputs = () => {
-    const nombreValue = nombre.value.trim();
-    const emailValue = email.value.trim();
-    const mensajeValue = mensaje.value.trim();
+// const validateInputs = () => {
+//     const nombreValue = nombre.value.trim();
+//     const emailValue = email.value.trim();
+//     const mensajeValue = mensaje.value.trim();
 
-    if(nombreValue === ''){
-        setError(nombre, 'Por favor indica tu nombre.');
-    }else{
-        setSuccess(nombre);
-    }
+//     if(nombreValue === ''){
+//         setError(nombre, 'Por favor indica tu nombre.');
+//     }else{
+//         setSuccess(nombre);
+//     }
 
-    if(emailValue === ''){
-        setError(email, 'Por favor indica tu email.');
-    }else if (!isValidEmail(emailValue)){
-        setError(email, 'Por favor ingrese un email válido.');
-    }else{
-        setSuccess(email);
-    }
+//     if(emailValue === ''){
+//         setError(email, 'Por favor indica tu email.');
+//     }else if (!isValidEmail(emailValue)){
+//         setError(email, 'Por favor ingrese un email válido.');
+//     }else{
+//         setSuccess(email);
+//     }
 
-    if (mensajeValue === ''){
-        setError(mensaje, 'Por favor ingrese su consulta.');
-    }else{
-        setSuccess(mensaje);
-    }
-}
+//     if (mensajeValue === ''){
+//         setError(mensaje, 'Por favor ingrese su consulta.');
+//     }else{
+//         setSuccess(mensaje);
+//     }
+// }
 
 form.addEventListener('submit', e =>{
     e.preventDefault();
-    validateInputs();
+    //validateInputs();
 
     fetch("https://formsubmit.co/ajax/lcsgilardi@gmail.com",{
         method:'POST',
